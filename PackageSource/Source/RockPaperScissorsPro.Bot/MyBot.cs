9383cf24d7dd706e59db3426c356f5f2b0d52e84
Aaron Jensen <aaronjensen@gmail.com>
2009-02-26 23:38:25 -0800

    Starting work on sample bot

---
+++ PackageSource/Source/RockPaperScissorsPro.Bot/MyBot.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RockPaperScissorsPro.Bot
+{
+  public class MyBot : IRockPaperScissorsBot
+  {
+    public Move MakeMove(IPlayer you, IPlayer opponent, GameRules rules)
+    {
+    }
+  }
+}
