104009ebf8d311ca230a2a6fee1e055912c1b626
Aaron Jensen <aaronjensen@gmail.com>
2009-02-27 14:34:26 -0800

    Updating PackageSource

--- PackageSource/Source/RockPaperScissorsPro.Bot/MyBot.cs
+++ PackageSource/Source/RockPaperScissorsPro.Bot/MyBot.cs
@@ -2,13 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 
 namespace RockPaperScissorsPro.Bot
 {
   public class MyBot : IRockPaperScissorsBot
   {
+    private static readonly Random random = new Random(Guid.NewGuid().ToString().GetHashCode());
+    private static readonly Move[] moves = new Move[] { Move.Paper, Move.Rock, Move.Scissors };
+
     public Move MakeMove(IPlayer you, IPlayer opponent, GameRules rules)
     {
+      return moves[random.Next(3)];
     }
   }
 }
--- PackageSource/Source/RockPaperScissorsPro.Bot/MyBotFactory.cs
+++ PackageSource/Source/RockPaperScissorsPro.Bot/MyBotFactory.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Compete.Bot;
 
 namespace RockPaperScissorsPro.Bot
 {
   public class MyBotFactory : IBotFactory
   {
+    public IBot CreateBot()
+    {
+      return new MyBot();
+    }
   }
 }
