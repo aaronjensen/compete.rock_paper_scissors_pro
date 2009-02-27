2e108c1bc508acc4ce1b38bf86e512efbfee67e3
Jacob Lewallen <jlewallen@gmail.com>
2009-02-27 11:21:47 -0800

    Making sample Bot take ten minutes if we're named TakeForever

--- Source/RockPaperScissorsPro.AlwaysThrows/AlwaysThrowsBot.cs
+++ Source/RockPaperScissorsPro.AlwaysThrows/AlwaysThrowsBot.cs
@@ -14,10 +14,14 @@ namespace RockPaperScissorsPro.AlwaysThrows
 
   public class AlwaysThrowsBot : IRockPaperScissorsBot
   {
-    readonly Random _random = new Random();
+    readonly Random _random = new Random(Guid.NewGuid().GetHashCode());
 
     public Move MakeMove(IPlayer you, IPlayer opponent, GameRules rules)
     {
+      if (GetType().Assembly.Location.Contains("TakeForever"))
+      {
+        System.Threading.Thread.Sleep(TimeSpan.FromMinutes(10));
+      }
       if (_random.NextDouble() > 0.5)
       {
         return Move.Paper;
