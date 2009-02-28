784b6e447e713900bf453c64a806f04cdfa0b02d
Aaron Jensen <aaronjensen@gmail.com>
2009-02-28 14:28:53 -0800

    Making package.cmd work and build the zips

--- PackageSource/setup.cmd
+++ PackageSource/setup.cmd
@@ -2,10 +2,11 @@
 
 if [%1] == [] GOTO usage
 if [%2] == [] GOTO usage
+if [%3] == [] GOTO usage
 
-echo @Tools\Compete.Uploader.exe http://local.compete.com Source\RockPaperScissorsPro.Bot\bin\Debug\RockPaperScissorsPro.Bot.dll %1 %2 > upload.cmd
+echo @Tools\Compete.Uploader.exe http://%1 Source\RockPaperScissorsPro.Bot\bin\Debug\RockPaperScissorsPro.Bot.dll %2 %3 > upload.cmd
 
 exit
 
 :usage
-echo Usage: setup.cmd {teamName} {password}
+echo Usage: setup.cmd {url} {teamName} {password}
--- package.cmd
+++ package.cmd
@@ -4,3 +4,15 @@ popd
 copy ..\compete\source\compete.uploader\bin\debug\compete.uploader.exe PackageSource\Tools
 copy ..\compete\source\compete.bot\bin\debug\compete.bot.dll PackageSource\Libraries
 copy Source\RockPaperScissorsPro.Round1\bin\debug\RockPaperScissorsPro.Round1.dll PackageSource\Libraries
+
+del /s /q Packages
+mkdir Packages
+
+pushd PackageSource
+call git clean -d -f
+..\Tools\zip.exe -r ..\Packages\Round1.zip *
+popd
+pushd Source\RockPaperScissorsPro.Round3\bin\debug
+..\..\..\..\Tools\zip.exe ..\..\..\..\Packages\Round2.zip RockPaperScissorsPro.Round2.dll
+..\..\..\..\Tools\zip.exe ..\..\..\..\Packages\Round3.zip RockPaperScissorsPro.Round3.dll
+popd
