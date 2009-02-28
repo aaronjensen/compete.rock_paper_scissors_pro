e8e8d20c305f79dc141cea5b9c3f1eb316da43cd
Aaron Jensen <aaronjensen@gmail.com>
2009-02-28 10:31:44 -0800

    Updating the sample

--- PackageSource/Source/RockPaperScissorsPro.Bot/MyBot.cs
+++ PackageSource/Source/RockPaperScissorsPro.Bot/MyBot.cs
@@ -9,7 +9,7 @@ namespace RockPaperScissorsPro.Bot
   public class MyBot : IRockPaperScissorsBot
   {
     private static readonly Random random = new Random(Guid.NewGuid().ToString().GetHashCode());
-    private static readonly Move[] moves = new Move[] { Move.Paper, Move.Rock, Move.Scissors };
+    private static readonly Move[] moves = new Move[] { Round1Move.Paper, Round1Move.Rock, Round1Move.Scissors };
 
     public Move MakeMove(IPlayer you, IPlayer opponent, GameRules rules, IGameLog log)
     {
--- package.cmd
+++ package.cmd
@@ -3,4 +3,4 @@ call build.cmd
 popd
 copy ..\compete\source\compete.uploader\bin\debug\compete.uploader.exe PackageSource\Tools
 copy ..\compete\source\compete.bot\bin\debug\compete.bot.dll PackageSource\Libraries
-copy Source\RockPaperScissorsPro\bin\debug\RockPaperScissorsPro.dll PackageSource\Libraries
+copy Source\RockPaperScissorsPro.Round1\bin\debug\RockPaperScissorsPro.Round1.dll PackageSource\Libraries
